f2c5743a51c87f248dbb66bf8f19f7a0d4f0c740
Thom Bruce <thom@thombruce.com>
2019-01-02 00:57:08 +0000

    build hugo site

---
+++ js/global.min.3061d517a5807a3c616f65e087f91e0cd4afdbcacaa1b212770bff60d5bdc9e5.js
@@ -0,0 +1,4 @@
+"use strict";var documentElement=document.getElementsByTagName("html")[0],clickEvent="ontouchstart"in window?"touchend":"click",classMethods=["remove","add"];function someControl(id,textArr,className){var el=document.getElementsByTagName("html")[0];var acbox=document.getElementById(id),textNode=acbox.firstChild,toggled=false;acbox.addEventListener(clickEvent,function(){var selector=Number((toggled=!toggled));textNode.data=textArr[selector];el.classList[classMethods[selector]](className);},false);}
+function addContrastControl(){someControl("contrast",["Add more contrast","Remove additional contrast"],"contrast");}
+function addInvertedControl(){someControl("invmode",["Inverted mode","Normal mode"],"inverted");}
+addContrastControl();addInvertedControl();
